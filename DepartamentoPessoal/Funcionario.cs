0c554bf302128b5722a5a80bf3a468eb5fce8baf
mateusice85 <mateusice85@gmail.com>
2019-10-22 10:59:05 -0300

    Início do projeto de departamento pessoal, cadastro de funcionario e calculo de salário

---
+++ DepartamentoPessoal/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DepartamentoPessoal
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            return (Salario - Imposto);
+        }
+
+        public void AlmentoDeSalario(double salario)
+        {
+            if (Salario == default) Salario = salario;
+            else Salario = Salario + (Salario * salario / 100.0);
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", R$"
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ DepartamentoPessoal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace DepartamentoPessoal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Funcionario p = new Funcionario();
+                Console.WriteLine("Digite os dados do Funcionario");
+                Console.Write("Nome: ");
+                p.Nome = Console.ReadLine();
+                Console.Write("Salário Bruto: ");
+                p.AlmentoDeSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                Console.Write("Imposto: ");
+                p.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine();
+                Console.WriteLine("Funcionário: " + p);
+
+                Console.WriteLine();
+                Console.Write("Deseja almentar o salario: ");
+                string resposta = Console.ReadLine().ToUpper();
+
+                if (resposta == "SIM")
+                {
+                    Console.WriteLine();
+                    Console.Write("Quantos % deseja aumentar: ");
+                    p.AlmentoDeSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                }   
+
+                Console.WriteLine();
+                Console.WriteLine("Funcionário: " + p);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("O valor digitado não é válido, tente novamente !");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("FIM");
+
+        }
+    }
+}
